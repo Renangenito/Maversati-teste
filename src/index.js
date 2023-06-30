3e749acaf457bb0b45d76ebe9110a6728e413efc
Renan Bezerra de Lima <77756047+Renangenito@users.noreply.github.com>
2023-06-30 13:18:00 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,24 +1,11 @@
-//import React from "react";
-//import ReactDOM from "react-dom";
-//import "./index.css";
-//import App from "./App";
-
-//ReactDOM.render(
-  //<React.StrictMode>
-   //<App />
-  //</React.StrictMode>,
-  //document.getElementById("root")
-//);
-
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import AppRoutes from './routes';
+import "./index.css";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppRoutes />
+    <App />
   </React.StrictMode>
 );
-
